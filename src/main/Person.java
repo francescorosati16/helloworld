eae076c37857b8e5d9657ccc86c8ba2e0fc420db
cesko16 <francesco.rosati16@outlook.it>
2022-03-29 10:47:33 +0200

    Secondo commit

---
+++ src/main/Person.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Person {
+	
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	private String surname;
+	
+}
